90ad3737e3444015d67dcc8c02fd3500d8764972
Miguel-Bernardino <142626169+Miguel-Bernardino@users.noreply.github.com>
2023-08-21 18:45:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <locale.h>
 
+typedef char tipo_carta;
+
+typedef struct cartaNo{
+    tipo_carta card;
+    cartaNo *prox;
+}cartaNo;
+
+typedef struct carta{
+    cartaNo *topo;
+}carta;
+
+typedef struct player{
+    carta deck;  
+}player;
+
 int main(){
     setlocale(LC_ALL,"Portuguese");
     
